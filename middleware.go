e596f95d111fe25ff8f743dc85de21d5044d7c60
aayushmanocha <aayush.manocha@gmail.com>
2025-09-28 22:58:52 -0400

    update cors config

--- middleware.go
+++ middleware.go
@@ -5,6 +5,7 @@ import "net/http"
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "https://go-pong-react-client-xsal4.ondigitalocean.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token")
 		w.Header().Set("Access-Control-Expose-Headers", "Link")
